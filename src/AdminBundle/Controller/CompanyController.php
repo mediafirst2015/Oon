b5394088b212da37447562ea4c5c6d6a28f2c4db
Maxim <bhd.m@ya.ru>
2015-05-19 18:18:09 +0300

    add admin controller & view for company

---
+++ src/AdminBundle/Controller/CompanyController.php
@@ -0,0 +1,97 @@
+<?php
+namespace AdminBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Component\Security\Core\SecurityContext;
+use Symfony\Component\Security\Core\Encoder\MessageDigestPasswordEncoder;
+use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\Company;
+use AppBundle\Form\CompanyType;
+
+/**
+ * Class CompanyController
+ * @package AppBundle\Controller
+ * @Route("/admin/company")
+ */
+class CompanyController extends Controller{
+    const ENTITY_NAME = 'Company';
+    /**
+     * @Security("has_role('ROLE_OPERATOR')")
+     * @Route("/", name="company_list")
+     * @Template()
+     */
+    public function listAction(){
+        $items = $this->getDoctrine()->getRepository('AppBundle:'.self::ENTITY_NAME)->findAll();
+
+        $paginator  = $this->get('knp_paginator');
+        $pagination = $paginator->paginate(
+            $items,
+            $this->get('request')->query->get('company', 1),
+            500
+        );
+
+        return array('pagination' => $pagination);
+    }
+
+    /**
+     * @Security("has_role('ROLE_OPERATOR')")
+     * @Route("/add", name="company_add")
+     * @Template()
+     */
+    public function addAction(Request $request){
+        $em = $this->getDoctrine()->getManager();
+        $item = new Company();
+        $form = $this->createForm(new CompanyType($em), $item);
+        $formData = $form->handleRequest($request);
+
+        if ($request->getMethod() == 'POST'){
+            if ($formData->isValid()){
+                $item = $formData->getData();
+                $em->persist($item);
+                $em->flush();
+                $em->refresh($item);
+                return $this->redirect($this->generateUrl('company_list'));
+            }
+        }
+        return array('form' => $form->createView());
+    }
+
+    /**
+     * @Security("has_role('ROLE_OPERATOR')")
+     * @Route("/edit/{id}", name="company_edit")
+     * @Template()
+     */
+    public function editAction(Request $request, $id){
+        $em = $this->getDoctrine()->getManager();
+        $item = $this->getDoctrine()->getRepository('AppBundle:'.self::ENTITY_NAME)->findOneById($id);
+        $form = $this->createForm(new CompanyType($em), $item);
+        $formData = $form->handleRequest($request);
+
+        if ($request->getMethod() == 'POST'){
+            if ($formData->isValid()){
+                $item = $formData->getData();
+                $em->flush($item);
+                $em->refresh($item);
+                return $this->redirect($this->generateUrl('company_list'));
+            }
+        }
+        return array('form' => $form->createView());
+    }
+
+    /**
+     * @Security("has_role('ROLE_OPERATOR')")
+     * @Route("/remove/{id}", name="company_remove")
+     */
+    public function removeAction(Request $request, $id){
+        $em = $this->getDoctrine()->getManager();
+        $item = $em->getRepository('AppBundle:'.self::ENTITY_NAME)->findOneById($id);
+        if ($item){
+            $em->remove($item);
+            $em->flush();
+        }
+        return $this->redirect($request->headers->get('referer'));
+    }
+}
